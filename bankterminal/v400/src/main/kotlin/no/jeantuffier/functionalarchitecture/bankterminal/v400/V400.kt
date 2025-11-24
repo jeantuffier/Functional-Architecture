2f192ab053440cf8efeb2c6221721215766eaf3d
Jean Tuffier <jean.tuffier@pm.me>
2025-11-24 22:46:56 +0100

    Add a fake sdk instance to the V400 class

--- bankterminal/v400/src/main/kotlin/no/jeantuffier/functionalarchitecture/bankterminal/v400/V400.kt
+++ bankterminal/v400/src/main/kotlin/no/jeantuffier/functionalarchitecture/bankterminal/v400/V400.kt
@@ -3,32 +3,37 @@ package no.jeantuffier.functionalarchitecture.bankterminal.v400
 import no.jeantuffier.functionalarchitecture.bankterminal.BankTerminal
 import org.koin.dsl.module
 
-class V400 : BankTerminal {
+class V400Sdk
+
+class V400BankTerminal : BankTerminal {
+
+    private var sdkInstance: V400Sdk? = null
 
     override fun openConnection() {
+        sdkInstance = V400Sdk()
         /**
-         * This assumes we use a provided SDK to interact with the bank terminal
+         * This assumes we use sdkInstance to interact with the bank terminal
          */
         println("Connection established")
     }
 
     override fun pay(amount: Int) {
         /**
-         * This assumes we use a provided SDK to interact with the bank terminal
+         * This assumes we use sdkInstance to interact with the bank terminal
          */
         println("Transaction completed")
     }
 
     override fun closeSettlement() {
         /**
-         * This assumes we use a provided SDK to interact with the bank terminal
+         * This assumes we use sdkInstance to interact with the bank terminal
          */
         println("Settlement closed")
     }
 
     override fun closeConnection() {
         /**
-         * This assumes we use a provided SDK to interact with the bank terminal
+         * This assumes we use sdkInstance to interact with the bank terminal
          */
         println("Connection closed")
     }
@@ -36,6 +41,6 @@ class V400 : BankTerminal {
 
 val v400Module = module {
     factory<BankTerminal> {
-        V400()
+        V400BankTerminal()
     }
 }
